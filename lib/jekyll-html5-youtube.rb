14475f12c301705db49aed6c18f3f5c852a4aa61
Lorenzo Sapora <lorenzo.sapora@gmail.com>
2017-02-12 02:42:52 +0000

    Add render def, and output embed container.

--- lib/jekyll-html5-youtube.rb
+++ lib/jekyll-html5-youtube.rb
@@ -8,4 +8,13 @@ class YouTubeEmbed < Liquid::Tag
     @text = text
   end
 
+  def render(context)
+    youtube_url = "#{context[@text.strip]}"
+    youtube_url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
+    @youtube_id = $1
+
+    %Q{<div class='embed-container'><object data="http://www.youtube.com/embed/#{ @youtube_id }"></object></div>}
+  end
+
+  Liquid::Template.register_tag "youtube", self
 end
